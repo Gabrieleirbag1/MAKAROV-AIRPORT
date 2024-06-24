455e3cc5d7c8efed88b38224f9ec32717f4b05ff
Wewenito <owen.pichot@gmail.com>
2024-06-24 15:04:47 +0100

    COMMIT FINAL, TRANS SERVICES OK

--- MAKAROV/MAKAROV/settings.py
+++ MAKAROV/MAKAROV/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=y+q9i@6-k5k=)vk-j*etv+3^n)&@!w3hn@3q1u3$$^28%fb4@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*',
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
