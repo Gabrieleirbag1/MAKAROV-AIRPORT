9190f090ced6b42ad5c40f3c4164ba278b043b0c
Missclick <119498688+gabrieleirbag1@users.noreply.github.com>
2024-06-24 15:22:55 +0200

    front django

--- docker/djangopache-compose/MAKAROV/wsgi.py
+++ docker/djangopache-compose/MAKAROV/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for REST project.
+WSGI config for MAKAROV project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'REST.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MAKAROV.settings')
 
 application = get_wsgi_application()
